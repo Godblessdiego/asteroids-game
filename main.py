321880ae427e20e10a830f3f5564619fce334fb2
Diego Figueroa <diegofigueroanl@gmail.com>
2025-07-17 18:01:04 -0400

    feature(fix): I made the CPU usage slower by reducing the fps of the game

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0.0
+
 
 def main():
     while True:
@@ -10,6 +13,8 @@ def main():
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                fps = clock.tick(60)
+                dt = fps / 1000.0
                 return
     print('Starting Asteroids!')
     print('Screen width:', SCREEN_WIDTH)
