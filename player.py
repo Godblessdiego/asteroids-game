768693d5bea98b409940a0abe80861a2589b1261
Godblessdiego <diegofigueroanl@gmail.com>
2025-07-23 19:02:17 -0400

    fix(bug): Fix a bug that made the game not run properly

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ from circleshape import CircleShape
 from constants import *
 
 class Player(CircleShape):
-    def __init__(self, x, y):
+    def __init__(self, x, y, radius):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
