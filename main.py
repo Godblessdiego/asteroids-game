d0c233827fba37228ae3ea9590663b78c5a5648a
Diego Figueroa <diegofigueroanl@gmail.com>
2025-07-22 23:41:36 -0400

    feature(add): Adding the player to the game, and make it render in the main function

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-dt = 0.0
+
 
 def main():
-    while True:
-        screen.fill('black')
-        pygame.display.flip()
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y, PLAYER_RADIUS)
+    running = True
+    while running:
+        fps = clock.tick(60)
+        dt = fps / 1000.0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                fps = clock.tick(60)
-                dt = fps / 1000.0
-                return
+                running = False
+
+        screen.fill('black')
+        player.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
     print('Starting Asteroids!')
     print('Screen width:', SCREEN_WIDTH)
     print('Screen height:', SCREEN_HEIGHT)
-
-if __name__ == "__main__":
     main()
